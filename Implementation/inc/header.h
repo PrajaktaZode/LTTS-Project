3f54a01b939fd1ca1695295b994c1d5f83389644
PrajaktaZode <76157085+PrajaktaZode@users.noreply.github.com>
2021-07-07 00:11:16 +0530

    Add files via upload

---
+++ Implementation/inc/header.h
@@ -0,0 +1,16 @@
+#ifndef __HEADER_H__
+#define __HEADER_H__
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+
+int score_pos_r1(int s1);
+int score_neg_r1(int s2);
+int score_pos_r2(int s3);
+int score_neg_r2(int s4);
+int final_score(int m1, int m2, int m3);
+
+#endif
